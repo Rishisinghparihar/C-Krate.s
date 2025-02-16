67ed2db0a7b855e16cd8a7dbd2b89884996221bd
Rishisinghparihar <rsparihar876@gmail.com>
2025-02-16 22:23:13 +0530

    Remove unused components and implement profile and notes screens (i.e. tab navigation)

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,36 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Feather from "@expo/vector-icons/Feather";
+import Ionicons from "@expo/vector-icons/Ionicons";
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="home" size={20} color="black" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="allNotes"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="plus-square" size={20} color="black" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-circle-outline" size={24} color="black" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
